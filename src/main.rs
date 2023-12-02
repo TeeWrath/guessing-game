0be9460f860cf283a1dfc958fe1fe4955e2d363f
TeeWrath <subroto.2003@gmail.com>
2023-12-03 03:09:27 +0530

    feat: comparing generated number and guessed number

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 // Standard Library for input and output
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main(){
     println!("Welcome to Guessing the number!!");
 
+    // Generating a random number in the rang of 1 to 100
     let secret_number: i32 = rand::thread_rng().gen_range(1..=100);
 
     println!("The secret number generated is : {secret_number}");
@@ -19,5 +21,15 @@ fn main(){
             .read_line(&mut guess)
             .expect("The number should be an integer");
     
+    // Converting guess String to a 32 bit signed integer
+    let guess : i32 = guess.trim().parse().expect("enter an integer number");
+
     println!("The number you guessed was {guess}");
+
+    // Comparing generated number and guessed number
+    match guess.cmp(&secret_number){
+        Ordering::Less => println!("Too Small"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Equal => println!("You win!")
+    }
 }
