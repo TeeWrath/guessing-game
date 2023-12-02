28f75fd990579c825a320c52922478d346aa1197
TeeWrath <subroto.2003@gmail.com>
2023-12-03 02:45:08 +0530

    feat: code for user to input a guessed number

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+fn main(){
+    println!("Welcome to Guessing the number!!");
+
+    println!("Kindly enter the number you guess!!");
+
+    let mut guess = String::new();
+    
+    io::stdin()
+            .read_line(&mut guess)
+            .expect("The number should be an integer");
+    
+    println!("The number you guessed was {guess}");
 }
