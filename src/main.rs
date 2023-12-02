5bc7c9870ceaacc3d65c7c3b742622e5d6a2f63e
TeeWrath <subroto.2003@gmail.com>
2023-12-03 03:03:49 +0530

    feat: generating secret number

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
+// Standard Library for input and output
 use std::io;
+use rand::Rng;
 
 fn main(){
     println!("Welcome to Guessing the number!!");
 
+    let secret_number: i32 = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number generated is : {secret_number}");
+
     println!("Kindly enter the number you guess!!");
 
-    let mut guess = String::new();
+    // Creating a new mutable variable
+    let mut guess: String = String::new(); // new is associate function which creates a new empty string
     
+    // input functions calling
     io::stdin()
             .read_line(&mut guess)
             .expect("The number should be an integer");
